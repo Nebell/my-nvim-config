d1afd3f7e2fa9898b75fbd5bfedb24b73acf3a5b
Nebell <a1322049727@foxmail.com>
2023-01-04 12:13:52 +0800

    leap.nvim argtextobj

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,11 +2,10 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-    use 'preservim/nerdtree'
     use {'neoclide/coc.nvim', branch = 'release'}
-    use 'tpope/vim-surround'
-    use 'itchyny/lightline.vim'
-    use 'skywind3000/vim-terminal-help'
+    use 'tpope/vim-commentary' -- commentary
+    use 'tpope/vim-surround' -- surround e.g. () []
+    use 'itchyny/lightline.vim' -- status bar
 
     -- theme
     use ({ 'projekt0n/github-nvim-theme' })
@@ -16,10 +15,10 @@ return require('packer').startup(function(use)
 
     -- file explorer
     use {
-      'nvim-tree/nvim-tree.lua',
-      requires = {
-        'nvim-tree/nvim-web-devicons', -- optional, for file icons
-      }
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        }
     }
 
     use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
@@ -27,9 +26,16 @@ return require('packer').startup(function(use)
     -- fuzzy search
     use {'Yggdroot/LeaderF', run = ':LeaderfInstallCExtension'}
 
-    -- float terminal
-    use 'voldikss/vim-floaterm'
+    -- terminal 
+    use 'voldikss/vim-floaterm'  -- float terminal
+    use 'skywind3000/vim-terminal-help' -- terminal
 
     -- debug
-    use "puremourning/vimspector"
+    use 'puremourning/vimspector'
+
+    -- function argument modifier
+    use 'vim-scripts/argtextobj.vim'
+
+    -- motion
+    use 'ggandor/leap.nvim'
 end)
