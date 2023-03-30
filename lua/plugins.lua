26ac47a79594a51ee6792cf4df65efa3e6a4c0d8
tangguangbin <tang.packet@outlook.com>
2023-03-30 13:36:28 +0800

    remove go.nvim; add rust.vim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -149,18 +149,8 @@ local plugins = {
         end,
     },
     {
-        "ray-x/go.nvim",
-        dependencies = { -- optional packages
-            "ray-x/guihua.lua",
-            "neovim/nvim-lspconfig",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        config = function()
-            require("go").setup()
-        end,
-        -- event = { "CmdlineEnter" },
-        ft = { "go", 'gomod' },
-        build = function() require("go.install").update_all_sync() end, -- if you need to install/update all binaries
+        "rust-lang/rust.vim",
+        ft = "rs",
     },
 
     -- highlight and code structure
