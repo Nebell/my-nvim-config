b5248c95d44106490e71bfdb3d9721b4b7de2f2a
nebell <a1322049727@foxmail.com>
2023-06-27 11:56:33 +0800

    split the plugins into files; change configure file structure

---
+++ lua/plugins/tabline.lua
@@ -0,0 +1,52 @@
+----------------------- bufferline -------------------
+local bufclose = function(bufnum) require('bufdelete').bufdelete(bufnum, true) end
+local config = {
+    options = {
+        diagnostics = { "nvim_lsp", "coc" },
+        offsets = { {
+            filetype = "NvimTree",
+            text = function() return vim.fn.getcwd() end,
+            highlight = "Directory",
+            text_align = "left"
+        } },
+        close_command = bufclose,
+        right_mouse_command = bufclose,
+        left_mouse_command = "buffer %d",
+        middle_mouse_command = bufclose,
+        always_show_bufferline = true,
+        hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' }
+        },
+        indicator = {
+            style = "none"
+        },
+        separator_style = "slant",
+        modified_icon = '㋡',
+    }
+}
+local function setup()
+    require('utils').async_run(function()
+        require("bufferline").setup(config)
+    end)
+end
+
+-------------- tabline -------------------
+return {
+    'akinsho/bufferline.nvim',
+    version = "v3.*",
+    event = "BufReadPre",
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        -- delete a buffer without mess up the layout
+        { 'famiu/bufdelete.nvim', module = 'bufferline' },
+    },
+    config = setup,
+    keys = {
+        { "<A-Left>",  "<cmd>BufferLineCyclePrev<CR>", mode = { "n", "v", "i" }, { silent = true } },
+        { "<A-Right>", "<cmd>BufferLineCycleNext<CR>", mode = { "n", "v", "i" }, { silent = true } },
+        { "<C-H>",     "<cmd>BufferLineCyclePrev<CR>", mode = { "n", "v", "i" }, { silent = true } },
+        { "<C-L>",     "<cmd>BufferLineCycleNext<CR>", mode = { "n", "v", "i" }, { silent = true } },
+    }
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,64 +1,50 @@
-local theme = require('theme')
+--------------------  github theme -------------------
+local function theme_setup()
+    local _ = require("onenord.colors").load()
+    require('onenord').setup({
+        theme = "dark",  -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+        borders = true,  -- Split window borders
+        fade_nc = false, -- Fade non-current windows, making them more distinguishable
+        -- Style that is applied to various groups: see `highlight-args` for options
+        styles = {
+            comments = "NONE",
+            strings = "NONE",
+            keywords = "NONE",
+            functions = "NONE",
+            variables = "NONE",
+            diagnostics = "underline",
+        },
+        disable = {
+            background = false, -- Disable setting the background color
+            cursorline = false, -- Disable the cursorline
+            eob_lines = true,   -- Hide the end-of-buffer lines
+        },
+        -- Inverse highlight for different groups
+        inverse = {
+            match_paren = false,
+        },
+        custom_highlights = {
+                              -- Overwrite default highlight groups
+            ["@string"] = { fg = "#B0BEC5" },
+            ["@character"] = { fg = "#90A4AE" },
+        },
+        custom_colors = {}, -- Overwrite default colors
+    })
+end
+
+-------------- rainbow parenthese ----------------
+vim.g.rainbow_active = 1
+vim.g.rainbow_conf = {
+    -- gui color
+    guifgs = { 'darkgray', 'lightslategray', 'lightskyblue', 'Cornflowerblue', 'peachpuff', 'white' }
+}
+
 return {
-    ---------- status bar -------------
-    {
-        'nvim-lualine/lualine.nvim',
-        event = "VeryLazy",
-        dependencies = { 'SmiteshP/nvim-navic' },
-        config = theme.lualine_setup
-    },
-    {
-        'SmiteshP/nvim-navic',
-        dependencies = "neovim/nvim-lspconfig",
-        module = "nvim-navic",
-    },
-
-    ---------- dashboard -------------
-    {
-        'glepnir/dashboard-nvim',
-        event = 'VimEnter',
-        config = require('theme').dashboard_setup,
-        dependencies = { { 'nvim-tree/nvim-web-devicons' } }
-    },
-
     ---------- theme -------------
     {
         'rmehri01/onenord.nvim',
         event = "UIEnter",
-        config = theme.theme_setup
-    },
-
-    ---------- file explorer -------------
-    {
-        'nvim-tree/nvim-tree.lua',
-        cmd = "NvimTreeToggle",
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-        },
-        config = theme.nvim_tree_setup,
-        keys = {
-            { "<C-G>",       "<CMD>NvimTreeToggle<CR>",   mode = { "n", "i", "v" }, { silent = true } },
-            { "<Leader>loc", "<CMD>NvimTreeFindFile<CR>", mode = { "n", "i", "v" }, { silent = true } },
-        }
-    },
-
-    ---------- tabline -------------------
-    {
-        'akinsho/bufferline.nvim',
-        version = "v3.*",
-        event = "BufReadPre",
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-            -- delete a buffer without mess up the layout
-            { 'famiu/bufdelete.nvim', module = 'bufferline' },
-        },
-        config = theme.bufferline_setup,
-        keys = {
-            { "<A-Left>",  "<cmd>BufferLineCyclePrev<CR>", mode = { "n", "v", "i" }, { silent = true } },
-            { "<A-Right>", "<cmd>BufferLineCycleNext<CR>", mode = { "n", "v", "i" }, { silent = true } },
-            { "<C-H>",     "<cmd>BufferLineCyclePrev<CR>", mode = { "n", "v", "i" }, { silent = true } },
-            { "<C-L>",     "<cmd>BufferLineCycleNext<CR>", mode = { "n", "v", "i" }, { silent = true } },
-        }
+        config = theme_setup
     },
 
     ---------- rainbow parenthese -------------
