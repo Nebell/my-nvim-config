31f93b29c5ceb37579028d53cf91a4c2ada69991
Nebell <a1322049727@foxmail.com>
2023-06-30 17:18:03 +0800

    make mason use the github proxy host

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -18,7 +18,24 @@ local mason_config = {
             package_pending = "➜",
             package_uninstalled = "✗"
         }
-    }
+    },
+    github = {
+        -- The template URL to use when downloading assets from GitHub.
+        -- The placeholders are the following (in order):
+        -- 1. The repository (e.g. "rust-lang/rust-analyzer")
+        -- 2. The release version (e.g. "v0.3.0")
+        -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
+        download_url_template = "https://" .. vim.g.git_src_host .. "/%s/releases/download/%s/%s",
+    },
+    pip = {
+        -- Whether to upgrade pip to the latest version in the virtual environment before installing packages.
+        upgrade_pip = false,
+        -- These args will be added to `pip install` calls. Note that setting extra args might impact intended behavior
+        -- and is not recommended.
+        --
+        -- Example: { "--proxy", "https://proxyserver" }
+        install_args = {},
+    },
 }
 
 -- mason-lspconfig config
