f10445af49e90d0908740ed3deb71cf301d33f5e
Nebell <a1322049727@foxmail.com>
2023-06-30 09:19:07 +0800

    limits the height of which-key popup window

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -10,5 +10,14 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
+        layout = {
+            height = { min = 1, max = 10 },
+        },
+
+        triggers_blacklist = {
+            i = { 'j', 'k', ',' },
+            v = { 'j', 'k', 'w' },
+            n = { 'v', 'V', 'd', 'w' },
+        }
     }
 }
