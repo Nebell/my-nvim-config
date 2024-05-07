08ca65a06bdd0a5e57da360df5c4e2f6278034f1
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    changed gui fonts and added comment

--- lua/options.lua
+++ lua/options.lua
@@ -48,7 +48,8 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- gui
-vim.opt.guifont = "Hack Nerd Font Mono:h14,CaskaydiaCove Nerd Font Mono:h14,Noto Color Emoji:h14,Microsoft Yahei UI:h14,sans-serif:h14"
+-- guifont: use `:set guifont=*` to select font on gui neovim
+vim.opt.guifont = "Hack Nerd Font Mono:h14,CaskaydiaCove NFM:h14,Noto Color Emoji:h14,Microsoft Yahei UI:h14,sans-serif:h14"
 
 -- match
 vim.opt.showmatch = true
