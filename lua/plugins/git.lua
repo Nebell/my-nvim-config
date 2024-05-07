2a94250b15e525e2eaaf2a10a146427f9cae13c6
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    added fugitive blame shortkey and quit button

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,21 +6,36 @@ return {
         config = function() require('gitsigns').setup() end,
         event = "BufReadPost",
         keys = {
-            { "<Leader>bl", "<cmd>Gitsigns toggle_current_line_blame<CR>", { 'n', 'v' }, { silent = true, noremap = true } },
+            { "<Leader>bl", "<cmd>Gitsigns toggle_current_line_blame<CR>", { 'n', 'v' },
+                { silent = true, noremap = true } },
         },
     },
 
     ---------- git ------------------
     {
         'tpope/vim-fugitive',
-        cmd = {'Git', 'Gvdiffsplit', 'Ggrep'}
+        cmd = { 'Git', 'Gvdiffsplit', 'Ggrep' },
+        init = function()
+            -- close via shortkey 'q'
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = { 'fugitiveblame' },
+                callback = function(ev)
+                    vim.api.nvim_buf_set_keymap(0, 'n', 'q', "<CMD>quit<CR>", { noremap = true, silent = true })
+                end
+            })
+        end,
+        keys = {
+            { 'gl', function()
+                vim.cmd("Git blame")
+            end },
+        },
     },
 
     ---------- git diff -------------
     {
         'sindrets/diffview.nvim',
         dependencies = 'nvim-lua/plenary.nvim',
-        cmd = {"DiffviewOpen"},
+        cmd = { "DiffviewOpen" },
     },
 
     ---------- git branchs ----------
@@ -35,8 +50,16 @@ return {
         config = true,
         cmd = "Neogit",
         keys = {
-            { "gv", function() require('neogit').open({ kind = "auto" }) end,  { 'n', 'v' }, { slient = true, noremap = true } },
-            { "gm", function() require('neogit').open({ "commit" }) end,       { 'n', 'v' }, { slient = true, noremap = true } },
+            { "gv", function() require('neogit').open({ kind = "auto" }) end, { 'n', 'v' },
+                {
+                    slient = true,
+                    noremap = true
+                } },
+            { "gm", function() require('neogit').open({ "commit" }) end, { 'n', 'v' },
+                {
+                    slient = true,
+                    noremap = true
+                } },
         }
     }
 }
