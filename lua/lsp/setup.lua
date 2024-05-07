d036cec0540a91729730d442379de6b4b0e15d94
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    autoload lsp server with default config

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -127,6 +127,18 @@ function M.setup()
             lsp[srv].setup(cfg)
         end
     end
+
+    require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function(server_name)  -- default handler (optional)
+            require("lspconfig")[server_name].setup {
+                capabilities = capabilities
+            }
+        end,
+
+    }
 end
 
 local lspsaga_config = {
