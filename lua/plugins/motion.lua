d62baa31dae7edf24f73d7b7b9584802c28da87d
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    remove leap.nvim

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -1,16 +1,4 @@
 return {
-    ---------- motion -------------
-    {
-        'ggandor/leap.nvim',
-        event = "BufReadPost",
-        keys = {
-            { "z", "<Plug>(leap-forward-to)" },
-            { "Z", "<Plug>(leap-backward-to)" },
-        },
-        config = function() require('leap') end
-    },
-
-
     ---------- block move -------------
     {
         'booperlv/nvim-gomove',
@@ -32,15 +20,16 @@ return {
     --------- operate motion ------------
     {
         "folke/flash.nvim",
-        event = "VeryLazy",
-        ---@type Flash.Config
         opts = {},
         -- stylua: ignore
         keys = {
-            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-            { "S", mode = { "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S", mode = { "o", "x" },      function() require("flash").treesitter() end,        desc =
+            "Flash Treesitter" },
+            { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end,
+                                                                                                      desc =
+                "Treesitter Search" },
         },
     }
 }
