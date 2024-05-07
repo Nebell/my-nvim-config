d027604a99a5420a04180e86a73f945362921a2c
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    added flash.nvim

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -29,4 +29,18 @@ return {
         end
     },
 
+    --------- operate motion ------------
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S", mode = { "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        },
+    }
 }
