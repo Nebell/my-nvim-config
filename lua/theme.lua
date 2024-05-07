6beb742ed44c35a737609521e07f52ce095a8c78
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    dashboard

--- lua/theme.lua
+++ lua/theme.lua
@@ -115,4 +115,36 @@ function Theme.bufferline_setup()
     end)
 end
 
+local dashboard_config = {
+    theme = 'hyper',
+    config = {
+        week_header = {
+            enable = true,
+        },
+        shortcut = {
+            { desc = ' Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+                icon = ' ',
+                icon_hl = '@variable',
+                desc = 'Files',
+                group = 'Label',
+                action = 'Telescope find_files',
+                key = 'p',
+            },
+            {
+                desc = ' dotfiles',
+                group = 'Number',
+                action = 'Telescope dotfiles',
+                key = 'd',
+            },
+        },
+    },
+
+}
+
+-- dashboard
+function Theme.dashboard_setup()
+    require('dashboard').setup(dashboard_config)
+end
+
 return Theme
