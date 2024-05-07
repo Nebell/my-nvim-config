47fbfb8dabf95d6ebd80e1c51a50c5e4bada1429
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    more efficient when cwd

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -4,7 +4,7 @@ vim.g.loaded_netrwPlugin = 1
 local function setup()
     require("nvim-tree").setup({
         sync_root_with_cwd = true,
-        respect_buf_cwd = true,
+        respect_buf_cwd = false,
         update_focused_file = {
             enable = true,
             update_root = true
