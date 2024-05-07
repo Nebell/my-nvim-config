73382fbb072357a69b23c41555596e14cac59233
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    added color pairs

---
+++ lua/plugins/rainbow.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        "HiPhish/nvim-ts-rainbow2",
+        event = "BufReadPost",
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter'
+        },
+        init = function()
+            vim.cmd([[highlight TSRainbowRed guifg=#8491c3 ctermfg=LightBlue ]])
+            vim.cmd([[highlight TSRainbowYellow guifg=#698aab ctermfg=Brown ]])
+            vim.cmd([[highlight TSRainbowBlue guifg=#4c6cb3 ctermfg=Cyan ]])
+            vim.cmd([[highlight TSRainbowOrange guifg=#93b881 ctermfg=Brown ]])
+            -- vim.cmd([[highlight TSRainbowGreen guifg= ctermfg=]])
+            vim.cmd([[highlight TSRainbowViolet guifg=#e17b34 ctermfg=White ]])
+            -- vim.cmd([[highlight TSRainbowCyan guifg= ctermfg=]])
+        end,
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                rainbow = {
+                    enable = true,
+                    -- list of languages you want to disable the plugin for
+                    disable = { 'jsx', 'cpp' },
+                    -- Which query to use for finding delimiters
+                    query = 'rainbow-parens',
+                    -- Highlight the entire buffer all at once
+                    -- strategy = require('ts-rainbow').strategy.global,
+                    -- strategy = require('ts-rainbow').strategy.local,
+                }
+            }
+        end
+    },
+}
