f63115176c624c65d03f40bd5c96340da182b86a
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    added formatter plugin `conform.nvim` which is able to fallback to lsp

---
+++ lua/plugins/format.lua
@@ -0,0 +1,34 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = { { -- Customize or remove this keymap to your liking
+        "gf",
+        function()
+            require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+    },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            python = { "isort", "black" },
+            javascript = { { "prettierd", "prettier" } },
+            json = { "jq" },
+            protobuf = { "buf" },
+        },
+
+        -- Set up format-on-save
+        -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
+
+        -- Customize formatters
+        formatters = {},
+    },
+    init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+}
