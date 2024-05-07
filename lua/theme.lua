c3a5a824ba2c0c2c3a5d19f6762b5c08509faf22
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    added autocmd to make nvim-tree auto-close when it's the last one window

--- lua/theme.lua
+++ lua/theme.lua
@@ -25,7 +25,8 @@ function Theme.theme_setup()
         inverse = {
             match_paren = false,
         },
-        custom_highlights = { -- Overwrite default highlight groups
+        custom_highlights = {
+                              -- Overwrite default highlight groups
             ["@string"] = { fg = "#B0BEC5" },
             ["@character"] = { fg = "#90A4AE" },
         },
@@ -153,6 +154,25 @@ function Theme.nvim_tree_setup()
     })
 end
 
+-- auto close
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#ppwwyyxx
+vim.api.nvim_create_autocmd("QuitPre", {
+    callback = function()
+        local invalid_win = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+            if bufname:match("NvimTree_") ~= nil then
+                table.insert(invalid_win, w)
+            end
+        end
+        if #invalid_win == #wins - 1 then
+            -- Should quit, so we close all invalid windows.
+            for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
+        end
+    end
+})
+
 ------------------- bufferline -------------------
 local bufclose = function(bufnum) require('bufdelete').bufdelete(bufnum, true) end
 Theme.bufferline_config = {
@@ -212,7 +232,6 @@ local dashboard_config = {
             },
         },
     },
-
 }
 
 function Theme.dashboard_setup()
