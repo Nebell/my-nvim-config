bfa4ef4a41b74cd037efb6dae4107624c789956f
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    added osc52 plugins

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -39,5 +39,28 @@ return {
             end
             require("yanky").setup(yanky_config)
         end,
-    }
+    },
+    {
+        'ojroques/nvim-osc52',
+        event = "TextYankPost",
+        init = function()
+            local copy = function()
+                -- if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+                --     require('osc52').copy_register('+')
+                -- end
+
+                require('osc52').copy_register('0')
+            end
+
+            vim.api.nvim_create_autocmd('TextYankPost', { callback = copy })
+        end,
+        config = function()
+            require('osc52').setup {
+                max_length = 0,           -- Maximum length of selection (0 for no limit)
+                silent = true,           -- Disable message on successful copy
+                trim = true,             -- Trim surrounding whitespaces before copy
+                tmux_passthrough = false, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
+            }
+        end
+    },
 }
