2d46567e380005a1b1d2c51601bb1e89e4dbe888
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    change theme to onenord; added toggleTerm

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,12 +1,35 @@
 Theme = {}
 
 -- github theme
-function Theme.github_setup()
-    require('github-theme').setup({
-        theme_style = "dark",
-        sidebars = { "terminal", "packer" },
-        -- dark_sidebar = false,
-        dark_float = true
+function Theme.theme_setup()
+    local colors = require("onenord.colors").load()
+    require('onenord').setup({
+        theme = "dark", -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+        borders = true, -- Split window borders
+        fade_nc = false, -- Fade non-current windows, making them more distinguishable
+        -- Style that is applied to various groups: see `highlight-args` for options
+        styles = {
+            comments = "NONE",
+            strings = "NONE",
+            keywords = "NONE",
+            functions = "NONE",
+            variables = "NONE",
+            diagnostics = "underline",
+        },
+        disable = {
+            background = false, -- Disable setting the background color
+            cursorline = false, -- Disable the cursorline
+            eob_lines = true, -- Hide the end-of-buffer lines
+        },
+        -- Inverse highlight for different groups
+        inverse = {
+            match_paren = false,
+        },
+        custom_highlights = { -- Overwrite default highlight groups
+            ["@string"] = { fg = "#B0BEC5"},
+            ["@character"] = { fg = "#90A4AE" },
+        },
+        custom_colors = {}, -- Overwrite default colors
     })
 end
 
@@ -65,7 +88,7 @@ function Theme.lualine_setup()
 end
 
 -- symbols outline
-vim.g.vista_icon_indent = { "╰─▸ ", "├─▸ " }
+-- vim.g.vista_icon_indent = { "╰─▸ ", "├─▸ " }
 -- vim.g["vista#renderer#enable_icon"] = 1
 -- vim.g["vista#renderer#icons"]["function"] = "\\uf794"
 -- vim.g["vista#renderer#icons"]["variable"] = "\\uf71b"
