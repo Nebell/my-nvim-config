161df958a5e469ba5701014cf39112ffd06e80cd
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    change basic keymap

--- lua/keyset.lua
+++ lua/keyset.lua
@@ -1,11 +1,5 @@
-local keyset = vim.keymap.set
+local keymap = vim.keymap.set
 
-keyset({"n", "v"}, "<C-K>", "15k")
-keyset({"n", "v"}, "<C-J>", "15j")
---- indent
--- Ctrl-I will act as <Tab>: https://github.com/neovim/neovim/issues/5916
--- keyset("n", "<TAB>", ">>")
-keyset("n", "<S-TAB>", "<<")
-keyset("v", "<TAB>", ">")
-keyset("v", "<S-TAB>", "<")
-
+local opts = { silent = true, noremap = true }
+keymap({ "n", "v" }, "<C-K>", "15k", opts)
+keymap({ "n", "v" }, "<C-J>", "15j", opts)
