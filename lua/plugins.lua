fd73e6c1ed397f972b82ab71ff7c6d26c8364c82
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    first commit

---
+++ lua/plugins.lua
@@ -0,0 +1,37 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'preservim/nerdtree'
+    use {'neoclide/coc.nvim', branch = 'release'}
+    use 'tpope/vim-surround'
+    use 'itchyny/lightline.vim'
+    use 'skywind3000/vim-terminal-help'
+    use({
+        'glepnir/zephyr-nvim',
+        requires = { 'nvim-treesitter/nvim-treesitter', opt = true },
+    })
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    -- rainbow parenthese
+    use 'luochen1990/rainbow'
+
+    -- file explorer
+    use {
+      'nvim-tree/nvim-tree.lua',
+      requires = {
+        'nvim-tree/nvim-web-devicons', -- optional, for file icons
+      }
+    }
+
+    use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+
+    -- fuzzy search
+    use {'Yggdroot/LeaderF', run = ':LeaderfInstallCExtension'}
+
+    -- float terminal
+    use 'voldikss/vim-floaterm'
+end)
