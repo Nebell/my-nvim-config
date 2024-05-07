908d168aa92ebdf4af41fa5b1b1b8251e99bf019
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    change yank features to yanky.nvim

--- lua/autocmd/highlights.lua
+++ lua/autocmd/highlights.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_augroup("custom.highlights", {clear = true})
 
 -- highlight yanks
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group    = 'custom.highlights',
-    pattern  = '*',
-    callback = function() vim.highlight.on_yank { timeout = 200 } end
-})
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--     group    = 'custom.highlights',
+--     pattern  = '*',
+--     callback = function() vim.highlight.on_yank { timeout = 200 } end
+-- })
