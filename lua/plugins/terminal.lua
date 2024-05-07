0a8430e8c22fdd255bbd018c50cae099c95a3aa9
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    switch terminal by <Leader>t[num]

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,13 +1,28 @@
----------- terminal -------------
+local keys = {
+    { "<C-T>", "<CMD>ToggleTerm<CR>",            mode = { "i", "n", "v" }, { silent = true } },
+    { "<C-T>", "<C-\\><C-n><CMD>ToggleTerm<CR>", mode = 't',               { silent = true } },
+    { "<ESC>", "<C-\\><C-n>",                    mode = 't',               { silent = true } },
+}
+
+-- generate terminal keys
+for i = 1, 9, 1 do
+    keys[#keys + 1] = {
+        string.format("<Leader>t%d", i),
+        function() vim.cmd({ cmd = 'ToggleTerm', args = { tostring(i) } }) end,
+        mode = { 'n', 'v' },
+        { silent = true },
+        desc = string.format("Terminal [%d]", i)
+    }
+end
+
 return {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    opts = {
-        direction = "float",
-    },
-    keys = {
-        { "<C-T>", "<CMD>ToggleTerm<CR>",            mode = { "i", "n", "v" }, { silent = true } },
-        { "<C-T>", "<C-\\><C-n><CMD>ToggleTerm<CR>", mode = 't',               { silent = true } },
-        { "<ESC>", "<C-\\><C-n>",                    mode = 't',               { silent = true } },
+    {
+        ---------- terminal -------------
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        opts = {
+            direction = "float",
+        },
+        keys = keys,
     }
 }
