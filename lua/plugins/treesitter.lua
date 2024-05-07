05ae875c5ad0418f104f50e12c414bf65d5e106b
Nebell <a1322049727@foxmail.com>
2024-05-07 13:44:35 +0800

    treesitter auto install set to false

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ local treesitter_config = {
     sync_install = false,
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+    auto_install = false,
     highlight = {
         enable = true,
         -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
