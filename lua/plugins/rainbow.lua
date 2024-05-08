f5f82d078339fff8929a3b526bf19f59c265dd9b
Nebell <a1322049727@foxmail.com>
2024-05-08 19:41:53 +0800

    move treesitter-ts-rainbow2 to rainbow-delimiters.nvim

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -1,30 +1,55 @@
+local function setup_colors()
+    vim.cmd([[highlight TSRainbowRed guifg=#8491c3 ctermfg=LightBlue ]])
+    vim.cmd([[highlight TSRainbowYellow guifg=#698aab ctermfg=Brown ]])
+    vim.cmd([[highlight TSRainbowBlue guifg=#4c6cb3 ctermfg=Cyan ]])
+    vim.cmd([[highlight TSRainbowOrange guifg=#339999 ctermfg=Brown ]])
+    -- vim.cmd([[highlight TSRainbowGreen guifg= ctermfg=]])
+    vim.cmd([[highlight TSRainbowViolet guifg=#93b881 ctermfg=White ]])
+    vim.cmd([[highlight TSRainbowCyan guifg=#CCCCCC ctermfg=Cyan ]])
+end
+
 return {
     {
-        "HiPhish/nvim-ts-rainbow2",
+        "hiphish/rainbow-delimiters.nvim",
+        -- enabled = false,
         event = "BufReadPost",
         dependencies = {
             'nvim-treesitter/nvim-treesitter'
         },
         config = function()
-            vim.cmd([[highlight TSRainbowRed guifg=#8491c3 ctermfg=LightBlue ]])
-            vim.cmd([[highlight TSRainbowYellow guifg=#698aab ctermfg=Brown ]])
-            vim.cmd([[highlight TSRainbowBlue guifg=#4c6cb3 ctermfg=Cyan ]])
-            vim.cmd([[highlight TSRainbowOrange guifg=#339999 ctermfg=Brown ]])
-            -- vim.cmd([[highlight TSRainbowGreen guifg= ctermfg=]])
-            vim.cmd([[highlight TSRainbowViolet guifg=#93b881 ctermfg=White ]])
-            vim.cmd([[highlight TSRainbowCyan guifg=#CCCCCC ctermfg=Cyan ]])
+            -- This module contains a number of default definitions
+            local rainbow_delimiters = require 'rainbow-delimiters'
 
-            require('nvim-treesitter.configs').setup {
-                rainbow = {
-                    enable = true,
-                    -- list of languages you want to disable the plugin for
-                    disable = { 'jsx', 'cpp' },
-                    -- Which query to use for finding delimiters
-                    query = 'rainbow-parens',
-                    -- Highlight the entire buffer all at once
-                    -- strategy = require('ts-rainbow').strategy.global,
-                    -- strategy = require('ts-rainbow').strategy.local,
-                }
+            setup_colors()
+            ---@type rainbow_delimiters.config
+            vim.g.rainbow_delimiters = {
+                strategy = {
+                    [''] = rainbow_delimiters.strategy['global'],
+                    vim = rainbow_delimiters.strategy['local'],
+                },
+                query = {
+                    [''] = 'rainbow-delimiters',
+                    lua = 'rainbow-blocks',
+                },
+                priority = {
+                    [''] = 110,
+                    lua = 210,
+                },
+                highlight = {
+                    -- 'RainbowDelimiterRed',
+                    -- 'RainbowDelimiterYellow',
+                    -- 'RainbowDelimiterBlue',
+                    -- 'RainbowDelimiterOrange',
+                    -- 'RainbowDelimiterGreen',
+                    -- 'RainbowDelimiterViolet',
+                    -- 'RainbowDelimiterCyan',
+                    'TSRainbowRed',
+                    'TSRainbowYellow',
+                    'TSRainbowBlue',
+                    'TSRainbowOrange',
+                    'TSRainbowViolet',
+                    'TSRainbowCyan',
+                },
             }
         end
     },
