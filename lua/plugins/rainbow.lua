b448f29f15d2c997b0ba27e40ae0e6018aa848af
Nebell <a1322049727@foxmail.com>
2024-05-08 19:57:39 +0800

    change color group

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -1,7 +1,7 @@
 local function setup_colors()
     vim.cmd([[highlight TSRainbowRed guifg=#8491c3 ctermfg=LightBlue ]])
     vim.cmd([[highlight TSRainbowYellow guifg=#698aab ctermfg=Brown ]])
-    vim.cmd([[highlight TSRainbowBlue guifg=#4c6cb3 ctermfg=Cyan ]])
+    vim.cmd([[highlight TSRainbowBlue guifg=#4699C9 ctermfg=Cyan ]])
     vim.cmd([[highlight TSRainbowOrange guifg=#339999 ctermfg=Brown ]])
     -- vim.cmd([[highlight TSRainbowGreen guifg= ctermfg=]])
     vim.cmd([[highlight TSRainbowViolet guifg=#93b881 ctermfg=White ]])
